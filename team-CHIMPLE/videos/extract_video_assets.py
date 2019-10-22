e2304e485189b70b6026d7092587553230942a0c
Jo Grimstad <jo.grimstad@xprize.org>
2019-10-22 20:54:55 +0800

    #7 Team Chimple: Extract a List of Videos
    - Added TODOs

---
+++ team-CHIMPLE/videos/extract_video_assets.py
@@ -0,0 +1,2 @@
+# TODO: extract from https://github.com/XPRIZE/GLEXP-Team-Chimple-goa/blob/master/goa/Resources/res/config/game_levels.json
+# TODO: extract from https://github.com/XPRIZE/GLEXP-Team-Chimple-goa/tree/master/goa/Resources/res/lang/swa/help
